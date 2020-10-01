03b9bb5e51cfa5ac549319a22285679d988104b8
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-09-30 21:49:55 -0300

    :sparkles: docker file created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [__dirname + './**/**/*entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
